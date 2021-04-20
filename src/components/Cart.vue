e4b417ad5589e95a565c54abd730ca122bee5f94
adoleba <ania.doleba@wp.pl>
2021-04-20 17:25:01 +0200

    static cart template

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,5 +1,45 @@
 <template>
-  <h3>Cart</h3>
+  <div class="container py-5">
+    <h3>Your cart</h3>
+
+    <div class="row py-5">
+      <div class="col-2 font-weight-bold">Product photo</div>
+      <div class="col-4 font-weight-bold">Product name</div>
+      <div class="col-2 font-weight-bold">Unit Price</div>
+      <div class="col-2 font-weight-bold">Quantity</div>
+      <div class="col-2 font-weight-bold">Value</div>
+    </div>
+
+    <div class="row pb-5">
+      <div class="col-2">Product photo</div>
+      <div class="col-4">Product name</div>
+      <div class="col-2">Unit Price</div>
+      <div class="col-2">Quantity</div>
+      <div class="col-2">Value</div>
+    </div>
+
+    <hr>
+
+    <div class="row justify-content-end py-5">
+      <div class="col-2  font-weight-bold">
+        Total value:
+      </div>
+      <div class="col-2">
+        Value
+      </div>
+    </div>
+
+    <div class="row py-5">
+    <div class="col-10">
+      <router-link class="btn btn-outline-secondary" :to="{ name: 'home' }">Continue shopping</router-link>
+    </div>
+    <div class="col-2">
+      <button class="btn btn-success rounded btn-lg">Checkout</button>
+    </div>
+  </div>
+
+  </div>
+
 </template>
 
 <script>
