6fbb536db5d2840e3d9c632014ff0ebb2ba0f668
adoleba <ania.doleba@wp.pl>
2021-05-13 13:18:33 +0200

    fixed rwd for OrderConfirm

--- src/components/OrderConfirm.vue
+++ src/components/OrderConfirm.vue
@@ -7,17 +7,17 @@
         </div>
       </div>
 
-      <div class="row justify-content-center p-4">
-        <div class="col-8 border rounded p-4">
+      <div class="row justify-content-center p-1 p-md-4">
+        <div class="col-8 border rounded p-1 p-md-4">
 
-          <div class="row p-4">
-            <div class="col-4">To pay: </div>
-            <div class="col-8"><span class="font-weight-bold">{{ (totalCost).toLocaleString().replace(',', ' ').replace('.', ',') }} zł</span></div>
+          <div class="row justify-content-center justify-content-md-start p-4">
+            <div class="col-sm col-md-4">To pay: </div>
+            <div class="col-sm col-md-8"><span class="font-weight-bold">{{ (totalCost).toLocaleString().replace(',', ' ').replace('.', ',') }} zł</span></div>
           </div>
 
           <div class="row px-4">
-            <div class="col-4">Delivery: </div>
-            <div class="col-8">{{ deliveryMethod }}</div>
+            <div class="col-sm col-md-4">Delivery: </div>
+            <div class="col-sm col-md-8">{{ deliveryMethod }}</div>
           </div>
 
           <hr />
@@ -27,39 +27,39 @@
           </div>
 
           <div v-if="invoice">
-            <div v-if="shippingData['companyName']" class="row px-4 py-1">
-              <div class="col-4">Company Name:</div>
-              <div class="col-8"> {{shippingData['companyName']}}</div>
+            <div class="row px-4 py-1">
+              <div class="col-sm col-md-4">Company Name:</div>
+              <div class="col-sm col-md-8"> {{shippingData['companyName']}}</div>
             </div>
 
-            <div v-if="shippingData['nip']" class="row px-4 py-1">
-              <div class="col-4">Nip Number:</div>
-              <div class="col-8"> {{shippingData['nip']}}</div>
+            <div class="row px-4 py-1">
+              <div class="col-sm col-md-4">Nip Number:</div>
+              <div class="col-sm col-md-8"> {{shippingData['nip']}}</div>
             </div>
 
-          </div>
-
-          <div v-if="shippingData['firstName']" class="row px-4 py-1">
-            <div class="col-4">Name:</div>
-            <div class="col-8"> {{shippingData['firstName']}} {{shippingData['lastName']}}</div>
           </div>
 
           <div class="row px-4 py-1">
-            <div class="col-4">Email:</div>
-            <div class="col-8"> {{shippingData['email']}}</div>
+            <div class="col-sm col-md-4">Name:</div>
+            <div class="col-sm col-md-8"> {{shippingData['firstName']}} {{shippingData['lastName']}}</div>
           </div>
 
           <div class="row px-4 py-1">
-            <div class="col-4">Address:</div>
-            <div class="col-8"> {{shippingData['street']}} {{shippingData['streetNumber']}}
+            <div class="col-sm col-md-4">Email:</div>
+            <div class="col-sm col-md-8"> {{shippingData['email']}}</div>
+          </div>
+
+          <div class="row px-4 py-1">
+            <div class="col-sm col-md-4">Address:</div>
+            <div class="col-sm col-md-8"> {{shippingData['street']}} {{shippingData['streetNumber']}}
               <span v-if="shippingData['apartmentNumber']">/{{shippingData['apartmentNumber']}}</span><br>
               {{shippingData['postalCode']}} {{shippingData['city']}}
             </div>
           </div>
 
           <div class="row px-4 py-1">
-            <div class="col-4">Comments:</div>
-            <div class="col-8">{{shippingData['comments']}}</div>
+            <div class="col-sm col-md-4">Comments:</div>
+            <div class="col-sm col-md-8">{{shippingData['comments']}}</div>
           </div>
 
 
