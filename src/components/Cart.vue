3190b5f204f44c459dc0c29e2ee879da5ac03d90
adoleba <ania.doleba@wp.pl>
2021-05-13 15:47:12 +0200

    fixed rwd for Cart

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,60 +1,92 @@
 <template>
-  <div class="container py-5">
+  <div class="container py-1 py-md-5">
     <div v-if="cart.cart.length>=1 ">
       <h3>Your cart</h3>
 
-      <div class="row py-5">
-        <div class="col-2 font-weight-bold">Product photo</div>
-        <div class="col-3 font-weight-bold">Product name</div>
-        <div class="col-2 font-weight-bold">Unit Price</div>
-        <div class="col-2 font-weight-bold">Quantity</div>
-        <div class="col-2 font-weight-bold">Value</div>
+      <div class="row py-2 py-md-5">
+        <div class="col-2 d-none d-md-block font-weight-bold">Product photo</div>
+        <div class="col-3 d-none d-md-block font-weight-bold">Product name</div>
+        <div class="col-2 d-none d-md-block font-weight-bold">Unit Price</div>
+        <div class="col-2 d-none d-md-block font-weight-bold">Quantity</div>
+        <div class="col-2 d-none d-md-block font-weight-bold">Value</div>
       </div>
 
       <div class="row pb-5" v-for="product in cart.cart" :key="product.details.id" :product="product">
-        <div class="col-2 align-self-center"><img :src="getImgUrl(product.details.id)" v-bind:alt="product.name" /></div>
-        <div class="col-3 align-self-center">
+
+        <div class="d-md-none">
+          <div class="row">
+            <div class="col-6 align-self-center">
+              <img :src="getImgUrl(product.details.id)" v-bind:alt="product.name" />
+            </div>
+            <div class="col-6 align-self-center">
+              <span class="font-weight-bold">Product: </span><br />
+              <router-link class="card-text" :to="{ name: 'productDetail' , params: {id: product.details.id}}">
+                {{ product.details.producer }} {{ product.details.name }}
+              </router-link>
+              <div class="align-self-center">
+                <span class="font-weight-bold">Unity price: </span><br />{{ product.details.price.toLocaleString().replace(',', ' ') }} zł
+              </div>
+              <div class="align-self-center py-2"><span class="font-weight-bold">Quantity: </span></div>
+              <div class="row">
+                <div class="col-8 col-sm-4 py-2">
+                  <i class="bi-dash" v-on:click="decreaseQuantity(product.details.id)" style="cursor: pointer"></i>
+                  <span class="border p-2 rounded">{{ product.quantity }}</span>
+                  <i class="bi-plus" v-on:click="increaseQuantity(product.details.id)" style="cursor: pointer"></i>
+                </div>
+                <div class="col-4 text-lg-left align-self-center py-2">
+                  <i class="bi-trash" v-on:click="removeFromCart(product.details.id)" style="cursor: pointer" title="Remove from cart"></i>
+                </div>
+              </div>
+              <div class="align-self-center">
+                <span class="font-weight-bold">Value: </span><br />{{ (product.details.price * product.quantity).toLocaleString().replace(',', ' ') }} zł
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-2 align-self-center d-none d-md-block"><img :src="getImgUrl(product.details.id)" v-bind:alt="product.name" /></div>
+        <div class="col-3 align-self-center d-none d-md-block">
           <router-link class="card-text" :to="{ name: 'productDetail' , params: {id: product.details.id}}">
             {{ product.details.producer }} {{ product.details.name }}
           </router-link>
         </div>
-        <div class="col-2 align-self-center">{{ product.details.price.toLocaleString().replace(',', ' ') }} zł</div>
+        <div class="col-2 align-self-center d-none d-md-block">{{ product.details.price.toLocaleString().replace(',', ' ') }} zł</div>
 
-        <div class="col-2 align-self-center">
+        <div class="col-2 align-self-center d-none d-md-block">
           <i class="bi-dash" v-on:click="decreaseQuantity(product.details.id)" style="cursor: pointer"></i>
           <span class="border p-2 rounded">{{ product.quantity }}</span>
           <i class="bi-plus" v-on:click="increaseQuantity(product.details.id)" style="cursor: pointer"></i>
         </div>
 
-        <div class="col-2 align-self-center">{{ (product.details.price * product.quantity).toLocaleString().replace(',', ' ') }} zł</div>
-        <div class="col-1 align-self-center">
+        <div class="col-2 align-self-center d-none d-md-block">{{ (product.details.price * product.quantity).toLocaleString().replace(',', ' ') }} zł</div>
+        <div class="col-1 text-lg-left align-self-center d-none d-md-block">
           <i class="bi-trash" v-on:click="removeFromCart(product.details.id)" style="cursor: pointer" title="Remove from cart"></i>
         </div>
       </div>
 
       <hr>
 
-      <div class="row justify-content-end py-5">
-        <div class="col-3  font-weight-bold">
+      <div class="row justify-content-md-end py-1 py-md-5">
+        <div class="col-6 col-md-3 font-weight-bold">
           Total value:
         </div>
-        <div class="col-3">
+        <div class="col-6 col-md-3 col-3">
           {{productsValue.toLocaleString().replace(',', ' ')}} zł
         </div>
       </div>
 
       <div class="row py-5">
-      <div class="col-10">
+      <div class="col-sm col-md-10">
         <router-link class="btn btn-outline-secondary" :to="{ name: 'home' }">Continue shopping</router-link>
       </div>
-      <div class="col-2">
-        <router-link class="btn btn-success rounded btn-lg" :to="{ name: 'order' }">Checkout</router-link>
+      <div class="col-sm col-sm-2 pt-4 pt-sm-0">
+        <router-link class="btn btn-success rounded" :to="{ name: 'order' }">Checkout</router-link>
       </div>
       </div>
     </div>
 
     <div v-else>
-      <div class="container py-5 my-2 text-center">
+      <div class="container py-1 py-md-5 my-2 text-center">
         <h3 class="pb-5 mt-5">Your cart is empty</h3>
         <router-link class="btn btn-success rounded btn-lg" :to="{ name: 'home' }">Go to shopping</router-link>
 
