5a3deb484c1c20ccd5b4637f3403af77da4472c1
adoleba <ania.doleba@wp.pl>
2021-05-13 12:19:27 +0200

    fixed rwd rof ProductPage

--- src/components/ProductDetail.vue
+++ src/components/ProductDetail.vue
@@ -1,41 +1,38 @@
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row text-center text-md-left">
 
-      <div class="col-6 justify-content-center">
+      <div class="col-12 col-md-6 justify-content-center">
         <img :src="getImgUrl(product.id)" v-bind:alt="product.name" class="card-img-top">
       </div>
 
-      <div class="col-6 justify-content-center py-5 pl-5">
+      <div class="col-12 col-md-6 justify-content-center py-5 pl-5">
         <h1 class="pb-5">{{product.producer}} {{product.name}}</h1>
         <div class="row">
-          <div class="col"><p class="font-weight-bold">Memory</p></div>
-          <div class="col-9">{{ product.memory }}</div>
+          <div class="col"><p><span class="font-weight-bold">Memory: </span>{{ product.memory }}</p></div>
         </div>
         <div class="row">
-          <div class="col"><p class="font-weight-bold">Screen</p></div>
-          <div class="col-9">{{ product.screen }}</div>
+          <div class="col"><p><span class="font-weight-bold">Screen: </span>{{ product.screen }}</p></div>
         </div>
         <div class="row">
-          <div class="col"><p class="font-weight-bold">Disk</p></div>
-          <div class="col-9">{{ product.disk }}</div>
+          <div class="col"><p><span class="font-weight-bold">Disk: </span>{{ product.disk }}</p></div>
         </div>
         <div class="row">
-          <div class="col"><p class="font-weight-bold">Processor</p></div>
-          <div class="col-9">{{ product.processor }}</div>
+          <div class="col"><p><span class="font-weight-bold">Processor: </span>{{ product.processor }}</p></div>
         </div>
 
-        <div class="row pl-3 pt-3">
+        <div class="row justify-content-center justify-content-md-start pl-3 pt-3">
           <h3 class="font-weight-bold">{{ product.price.toLocaleString().replace(',', ' ') }} zł</h3>
         </div>
 
-        <div class="row pb-5 pt-3">
-          <div class="col-6">
-            <input type="number" class="col-form-label" value="1" min="1" max="10" id="quantity"/>
+        <div class="row pt-3">
+          <div class="col">
+            <input type="number" class="col-form-label" style="max-width: 120px;" value="1" min="1" max="10" id="quantity"/>
           </div>
+        </div>
+        <div class="row pb-5 pt-3">
           <div class="col">
             <button class="btn btn-success rounded" v-on:click="addToCart(product)">Add to cart</button>
-
           </div>
         </div>
 
