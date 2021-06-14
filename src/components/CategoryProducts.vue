54bab517d4570573b3c4884b2757c341533f9943
adoleba <ania.doleba@wp.pl>
2021-06-14 10:58:25 +0200

    fixed missing product params in CategoryProducts component

--- src/components/CategoryProducts.vue
+++ src/components/CategoryProducts.vue
@@ -6,7 +6,8 @@
       <div class="card no-border">
         <img :src="getImgUrl(product.id)" v-bind:alt="product.name" class="card-img-top">
         <div class="card-body">
-          <router-link class="card-text" :to="{ name: 'productDetail' , params: {id: product.id}}">
+          <router-link class="card-text" :to="{ name: 'productDetail' , params:
+            {product: slugify(product.producer+'-'+product.name), id: product.id}}">
             {{ product.producer }} {{ product.name }}
           </router-link>
           <h5 class="card-title font-weight-bold">{{ product.price.toLocaleString().replace(',', ' ') }} zł</h5>
@@ -31,8 +32,10 @@ export default {
     return {toast};
   },
   data() {
+    const slugify = require('slugify');
     return {
       products: products,
+      slugify,
     };
   },
   created() {
