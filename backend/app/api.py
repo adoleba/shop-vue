9f5f83df21f0fdacc7f66304dc4c0e4147baa331
adoleba <ania.doleba@wp.pl>
2021-07-13 16:50:41 +0200

    create, update, delete and view list of products in API

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.encoders import jsonable_encoder
 
 from . import crud, database, models, schemas
 from .database import db_state_default
@@ -9,7 +10,7 @@ database.db.connect()
 database.db.create_tables([models.Product])
 database.db.close()
 
-app = FastAPI()
+app = FastAPI(title="VueShop Api")
 
 
 async def reset_db_state():
@@ -26,18 +27,60 @@ def get_db(db_state=Depends(reset_db_state)):
             database.db.close()
 
 
-@app.post("/products/", response_model=schemas.Product, dependencies=[Depends(get_db)],)
+@app.post(
+    "/products/",
+    response_model=schemas.Product,
+    dependencies=[Depends(get_db)],
+    tags=["products"],
+    summary="Add a new product"
+)
 def create_product(product: schemas.Product):
     return crud.create_product(product=product)
 
 
-@app.get("/products/", response_model=List[schemas.Product], dependencies=[Depends(get_db)])
+@app.get(
+    "/products/",
+    response_model=List[schemas.Product],
+    dependencies=[Depends(get_db)],
+    tags=["products"],
+    summary="View list of all products"
+)
 def read_products():
     products = crud.get_products()
     return products
 
 
-@app.get("/products/{product_id}", response_model=schemas.Product, dependencies=[Depends(get_db)])
+@app.put(
+    "/products/{product_id}",
+    response_model=schemas.Product,
+    dependencies=[Depends(get_db)],
+    tags=["products"],
+    summary="Update existing product"
+)
+def update_product(product_id: int, product_data: schemas.Product):
+    updated_product = crud.update_product(product_id, product_data)
+    return updated_product
+
+
+@app.delete(
+    "/products/{product_id}",
+    tags=["products"],
+    summary="Delete product"
+)
+def delete_product(product_id: int):
+    db_product = crud.delete_product(product_id=product_id)
+    if db_product is None:
+        raise HTTPException(status_code=404, detail="Product not found")
+    return {"detail": "Product deleted"}
+
+
+@app.get(
+    "/products/{product_id}",
+    response_model=schemas.Product,
+    dependencies=[Depends(get_db)],
+    tags=["products"],
+    summary="View product with given id"
+)
 def read_product(product_id: int):
     db_product = crud.get_product(product_id=product_id)
     if db_product is None:
--- backend/app/crud.py
+++ backend/app/crud.py
@@ -22,5 +22,25 @@ def create_product(product: schemas.Product):
     return db_product
 
 
+def update_product(product_id: int, product_data: schemas.Product):
+    product = models.Product.filter(models.Product.id == product_id).first()
+    product.producer = product_data.producer
+    product.name = product_data.name
+    product.description = product_data.description
+    product.category = product_data.category
+    product.memory = product_data.memory
+    product.screen = product_data.screen
+    product.price = product_data.price
+    product.processor = product_data.processor
+    product.disk = product_data.disk
+    product.onStock = product_data.onStock
+    product.save()
+    return product
+
+
+def delete_product(product_id: int):
+    return models.Product.delete().where(models.Product.id == product_id).execute()
+
+
 def get_product(product_id: int):
     return models.Product.filter(models.Product.id == product_id).first()
