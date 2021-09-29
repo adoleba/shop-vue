f5c22cc0083b780d62030300ba17cfa1ad1b7e73
adoleba <ania.doleba@wp.pl>
2021-09-29 14:34:24 +0200

    data from API in Category Product component

--- src/components/CategoryProducts.vue
+++ src/components/CategoryProducts.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="row">
 
+    <div v-if="!products.length">
+      <div class="container px-sm-5">
+          <div class="col-md-12 col-sm-12 col-xs-12">
+            <div class="text-center p-0 p-md-5">
+              <p>There are no products in category</p>
+            </div>
+          </div>
+      </div>
+    </div>
+
     <div v-for="product in products" :key="product.id" :products="products"
          class="col-sm-4 col-xs-12 text-center p-4 p-sm-2 p-lg-5">
       <div class="card no-border">
-        <img :src="getImgUrl(product.id)" v-bind:alt="product.name" class="card-img-top">
+        <img :src="getImgUrl(product.img_url)" v-bind:alt="product.name" class="card-img-top">
         <div class="card-body">
           <router-link class="card-text" :to="{ name: 'productDetail' , params:
-            {product: slugify(product.producer+'-'+product.name), id: product.id}}">
+          {product: slugify(product.producer+'-'+product.name), id: product.id}}">
             {{ product.producer }} {{ product.name }}
           </router-link>
           <h5 class="card-title font-weight-bold">{{ product.price.toLocaleString().replace(',', ' ') }} zł</h5>
@@ -21,31 +31,33 @@
 
 <script>
 import {useToast} from "vue-toastification";
-import products from "../data/products";
 import {Store} from "../store/store";
+import axios from "axios";
 
 export default {
   name: "CategoryProducts",
   props: ['categoryName'],
   setup() {
     const toast = useToast();
-    return {toast};
+    const slugify = require('slugify');
+    return {toast, slugify};
   },
   data() {
-    const slugify = require('slugify');
     return {
-      products: products,
-      slugify,
+      products: [],
     };
   },
-  created() {
+
+  mounted () {
     const categoryName = this.$route.params.categoryName;
-    this.products = this.products.filter(product => product.category === categoryName);
+    axios.get('http://localhost:8000/api/products/').then(response => {
+      this.products = response.data.filter(product => product.category === categoryName);
+    });
   },
 
   methods: {
-    getImgUrl(id) {
-      return require('../data/images/' + id + '.png');
+    getImgUrl(product) {
+      return require('../../public/' + product);
     },
     addToCart(product) {
       Store.addToCart(product);
